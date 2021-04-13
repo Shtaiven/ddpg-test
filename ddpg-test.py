4a3da37f01275b1a69ec186d89f4495246394689
Steven Eisinger <steven.a.eisinger@gmail.com>
2021-04-13 13:33:22 +0200

    Remove unused submodules, test robo_gym

--- ddpg-test.py
+++ ddpg-test.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import gym
-import pybulletgym
+import robo_gym
 import numpy as np
 
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
-env = gym.make("ReacherPyBulletEnv-v0")
+env = gym.make("EndEffectorPositioningUR10Sim-v0", ip='localhost', gui=True)
 
 # The noise objects for DDPG
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+# n_actions = env.action_space.shape[-1]
+# action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
-model.learn(total_timesteps=5000, log_interval=10)
+# model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+# model.learn(total_timesteps=5000, log_interval=10)
 # model.save("ddpg_pendulum")
 # env = model.get_env()
 
@@ -23,6 +23,7 @@ model.learn(total_timesteps=5000, log_interval=10)
 # model = DDPG.load("ddpg_pendulum")
 
 obs = env.reset()
+print("Env reset")
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
