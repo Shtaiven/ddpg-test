88a23c7f974a07fa00c056f135a0edfbd4f0e634
Steven Eisinger <steven.a.eisinger@gmail.com>
2021-04-15 19:06:07 +0200

    Added new hyperparameters

--- ddpg-test.py
+++ ddpg-test.py
@@ -3,14 +3,15 @@
 import gym
 import time
 import argparse
-import pybulletgym  # provides ReacherPyBulletEnv-v0
+# import pybulletgym  # provides ReacherPyBulletEnv-v0
+import pybullet_envs  # provides ReacherBulletEnv-v0
 import numpy as np
 
 from stable_baselines3 import DDPG, TD3
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 
-def main(load_model='', save_model='', timesteps=10000, use_td3=False):
-    env = gym.make('ReacherPyBulletEnv-v0')
+def main(load_model='', save_model='', timesteps=100000, use_td3=False):
+    env = gym.make('ReacherBulletEnv-v0')
     model_type = DDPG
     if use_td3:
         model_type = TD3
@@ -25,7 +26,32 @@ def main(load_model='', save_model='', timesteps=10000, use_td3=False):
         n_actions = env.action_space.shape[-1]
         action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
-        model = model_type("MlpPolicy", env, action_noise=action_noise, verbose=1)
+        # Set hyperparameters based on rl-baselines-zoo
+        # ReacherBulletEnv-v0:
+        #     env_wrapper: utils.wrappers.TimeFeatureWrapper
+        #     n_timesteps: !!float 1e6
+        #     policy: 'MlpPolicy'
+        #     gamma: 0.99
+        #     buffer_size: 1000000
+        #     noise_type: 'normal'
+        #     noise_std: 0.1
+        #     learning_starts: 10000
+        #     batch_size: 100
+        #     learning_rate: !!float 1e-3
+        #     train_freq: 1000
+        #     gradient_steps: 1000
+        #     policy_kwargs: "dict(layers=[400, 300])"
+        model = model_type("MlpPolicy",
+                           env,
+                           learning_rate=float(1e-3),
+                           learning_starts=1000000,
+                           gamma=0.99,
+                           buffer_size=1000000,
+                           batch_size=100,
+                           train_freq=1000,
+                           gradient_steps=1000,
+                           action_noise=action_noise,
+                           verbose=1)
 
         print(f'Training for {timesteps} timesteps')
         model.learn(total_timesteps=timesteps, log_interval=10)
@@ -57,7 +83,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', action='store', default='', metavar='FILENAME', help='load model data from specified zip file')
     parser.add_argument('-o', '--output', action='store', default='', metavar='FILENAME', help='save model data to specified zip file')
-    parser.add_argument('-t', '--timesteps', action='store', default='10000', metavar='N', help='train for N timesteps')
+    parser.add_argument('-t', '--timesteps', action='store', default='100000', metavar='N', help='train for N timesteps')
     parser.add_argument('--td3', action='store_true', help='use TD3 instead of DDPG')
     args = parser.parse_args()
 
